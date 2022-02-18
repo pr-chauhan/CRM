86bb5d78b2ae9e59ad11a6ba3090979c7d6a1c23
dharmesh3600 <dharmeshpiplani@gmail.com>
2022-02-18 15:50:58 +0530

    Commit by dharmesh

---
+++ Electra_WebApi/Reports/PrintInvoice.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrintInvoice.aspx.cs" Inherits="Electra_WebApi.PrintInvoice" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         
+        <div>
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server" Width="100%"></rsweb:ReportViewer>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Electra_WebApi/Reports/PrintInvoice.aspx.cs
@@ -0,0 +1,40 @@
+﻿using EntityClass;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Electra_WebApi
+{
+    public partial class PrintInvoice : System.Web.UI.Page
+    {
+      
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                int searchText = 0;
+
+                if (Request.QueryString["searchText"] != null)
+                {
+                    searchText = Convert.ToInt32( Request.QueryString["searchText"]);
+                }
+
+                List<Invoice> InvMain = null;
+                using (var _context = new CraModel())
+                {
+                    InvMain = _context.Invoices.Where(t => t.Invoice_ID == searchText).ToList();
+                    ReportViewer1.LocalReport.ReportPath = Server.MapPath("~/Reports/ReportInvoice.rdlc");
+                    ReportViewer1.LocalReport.DataSources.Clear();
+                    ReportDataSource rdc = new ReportDataSource("Invoice", InvMain);
+                    ReportViewer1.LocalReport.DataSources.Add(rdc);
+                    ReportViewer1.LocalReport.Refresh();
+                    ReportViewer1.DataBind();
+                }
+            }
+        }
+    }
+}
